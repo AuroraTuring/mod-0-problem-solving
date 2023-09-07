db56f4a5c79d889c47d729261d79ecb7aef31022
Aurora Sanderson <AuroraTuring@gmail.com>
2023-09-07 16:15:56 -0600

    Fix formatting

--- challenges.rb
+++ challenges.rb
@@ -33,8 +33,10 @@
 
 
 
+
 # Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. 
 # For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
+
 
 
 
